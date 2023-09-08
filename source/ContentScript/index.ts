f857cfa90749c0174a21c8d72f718eee1437027e
Jesse Szepieniec <jessems@gmail.com>
2023-09-08 08:48:04 +0100

    Got WS working

--- source/ContentScript/index.ts
+++ source/ContentScript/index.ts
@@ -1,3 +1,31 @@
+import {browser} from 'webextension-polyfill-ts';
+
 console.log('helloworld from content script');
+
+// Listen for messages from page context
+window.addEventListener(
+  'message',
+  (event) => {
+    // We only accept messages from ourselves
+    if (event.source !== window) return;
+
+    if (event.data.type && event.data.type === 'FROM_PAGE') {
+      // Send message to popup script
+      browser.runtime.sendMessage({g_ck: event.data.text});
+    }
+  },
+  false
+);
+
+// Inject script into the page
+const script = document.createElement('script');
+script.textContent = `
+    window.postMessage({
+      type: 'FROM_PAGE',
+      text: window.g_ck
+    }, '*');
+  `;
+(document.head || document.documentElement).appendChild(script);
+script.remove();
 
 export {};
--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -3,11 +3,103 @@ import {browser, Tabs} from 'webextension-polyfill-ts';
 
 import './styles.scss';
 
+// async function getToken(): Promise<string | null> {
+//   const url =
+//     'https://dev168935.service-now.com/sn_devstudio_/v1/get_publish_info.do';
+//   const response = await fetch(url, {
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json',
+//       Authorization: 'BasicCustom',
+//     },
+//   });
+//   const data = await response.json();
+
+//   return data.ck as string | null;
+// }
+
 function openWebPage(url: string): Promise<Tabs.Tab> {
   return browser.tabs.create({url});
 }
 
+interface Message {
+  g_ck: string;
+}
+
 const Popup: React.FC = () => {
+  // const [sessionToken, setSessionToken] = React.useState<string | null>(null);
+  const [csfrToken, setCSFRToken] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    const handleMessage = (message: Message): void => {
+      console.log('Received message:', message);
+      setCSFRToken(message.g_ck);
+      browser.storage.local.set({csfrToken: message.g_ck});
+    };
+
+    // Listen for messages from content script
+    browser.runtime.onMessage.addListener(handleMessage);
+
+    // Load the token from the extension's storage when the popup is opened
+    browser.storage.local.get('csfrToken').then((data) => {
+      setCSFRToken(data.csfrToken);
+    });
+
+    // Clean up the event listener when the component unmounts
+    return (): void => {
+      browser.runtime.onMessage.removeListener(handleMessage);
+    };
+  }, []);
+
+  // const handleGetToken = async (): Promise<void> => {
+  //   const newSessionToken = await getToken();
+  // setSessionToken(newSessionToken);
+  // console.log(newSessionToken);
+  // };
+
+  const handlePerformApiQuery = async (): Promise<void> => {
+    let token = csfrToken; // First, try to get the token from the state
+
+    // If the state doesn't have the token, try to get it from the storage
+    if (!token) {
+      const data = await browser.storage.local.get('csfrToken');
+      token = data.csfrToken;
+    }
+
+    if (token) {
+      const url =
+        'https://dev168935.service-now.com/api/now/table/incident/a83820b58f723300e7e16c7827bdeed2';
+      const response = await fetch(url, {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'BasicCustom',
+          'X-UserToken': token,
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+    }
+  };
+
+  const ws = new WebSocket('ws://localhost:8080');
+
+  ws.onopen = (): void => {
+    console.log('WebSocket is connected');
+  };
+
+  ws.onmessage = (event): void => {
+    console.log('Received data from server:', event.data);
+  };
+
+  ws.onerror = (error): void => {
+    console.error('WebSocket encountered an error:', error);
+  };
+
+  ws.onclose = (): void => {
+    console.log('WebSocket is closed');
+  };
+
   return (
     <section id="popup">
       <h2>WEB-EXTENSION-STARTER</h2>
@@ -20,34 +112,16 @@ const Popup: React.FC = () => {
       >
         Options Page
       </button>
-      <div className="links__holder">
-        <ul>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  'https://github.com/abhijithvijayan/web-extension-starter'
-                );
-              }}
-            >
-              GitHub
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  'https://www.buymeacoffee.com/abhijithvijayan'
-                );
-              }}
-            >
-              Buy Me A Coffee
-            </button>
-          </li>
-        </ul>
-      </div>
+      {/* <button id="options__button" type="button" onClick={handleGetToken}>
+        Get Token
+      </button> */}
+      <button
+        id="options__button"
+        type="button"
+        onClick={handlePerformApiQuery}
+      >
+        Perform API Query
+      </button>
     </section>
   );
 };
